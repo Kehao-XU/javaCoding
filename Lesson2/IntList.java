f2adb5baf2868648bfe2157f1122b8264eba1c8a
JustinianXU <25303050130@m.fudan.edu.cn>
2026-02-11 15:20:17 +0800

    Feature:Recursive&Iterative size calculation complete!

---
+++ Lesson2/IntList.java
@@ -0,0 +1,36 @@
+package Lesson2;
+
+public class IntList {
+    public int first;
+    public IntList rest;
+    
+    public IntList(int f,IntList r){
+        first=f;
+        rest=r;
+    }
+
+    public int size(){
+        /* Return the size of the list using recursion */
+        if(rest==null) return 1;
+        else return rest.size()+1;
+    }
+
+    public int iterativeSize(){
+        /* Return the size of the list using iteration */
+        IntList p = this;
+        int totalSize=0;
+        while(p!=null){
+            totalSize++;
+            p=p.rest;
+        }
+        return totalSize;
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(15,null);
+        L=new IntList(10,L);
+        L=new IntList(5,L);
+
+        System.out.println(L.iterativeSize());
+    }
+}
